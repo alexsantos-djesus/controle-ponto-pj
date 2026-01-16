f1d00a9b0db8b1374048368f770d9432939eaf49
alexsantos-djesus <alexsantos.djesus@gmail.com>
2026-01-15 22:26:27 -0300

    chore: setup inicial do projeto e ambiente

---
+++ src/lib/prisma.ts
@@ -0,0 +1,9 @@
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL!, });
+
+export const prisma = new PrismaClient({
+  adapter,
+});
